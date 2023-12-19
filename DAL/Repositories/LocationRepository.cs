c81bf128ca3651b8262bf76635b5e944bb77f80c
Rob <rob.fuller94@gmail.com>
2023-12-19 11:17:16 -0600

    Location Controller/repository

---
+++ DAL/Repositories/LocationRepository.cs
@@ -0,0 +1,53 @@
+﻿using DAL.Models;
+using DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class LocationRepository : Repository<Location>, ILocationRepository
+    {
+        public LocationRepository(ApplicationDbContext context) : base(context)
+        { }
+
+
+
+        public IEnumerable<Location> GetAllLocation()
+        {
+            return _appContext.Locations
+                .OrderBy(c => c.Id)
+                .ToList();
+        }
+
+        public Location GetLocationById(int Id)
+        {
+            return _appContext.Locations.FirstOrDefault(c => c.Id == Id);
+
+        }
+
+
+        public void AddLocation(Location location)
+        {
+            if (location == null)
+            {
+                throw new ArgumentNullException(nameof(location));
+            }
+
+            _appContext.Locations.Add(location);
+        }
+
+        public void UpdateLocation(Location location)
+        {
+            _appContext.Attach(location);
+            _appContext.Entry(location).State = EntityState.Modified;
+        }
+
+
+
+
+        private ApplicationDbContext _appContext => (ApplicationDbContext)_context;
+    }
+}
---
+++ QuickApp/Controllers/LocationController.cs
@@ -0,0 +1,110 @@
+﻿// ======================================
+// Author: Ebenezer Monney
+// Copyright (c) 2023 www.ebenmonney.com
+// 
+// ==> Gun4Hire: contact@ebenmonney.com
+// ======================================
+
+using AutoMapper;
+using DAL;
+using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QuickApp.Helpers;
+using QuickApp.ViewModels;
+using System;
+using System.Collections.Generic;
+
+namespace QuickApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LocationController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger _logger;
+        private readonly IEmailSender _emailSender;
+
+        public LocationController(IMapper mapper, IUnitOfWork unitOfWork, ILogger<LocationController> logger, IEmailSender emailSender)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+
+
+        // GET: api/values
+        [HttpGet("allLocations")]
+        public IActionResult GetAllLocations()
+        {
+            var allLocations = _unitOfWork.Locations.GetAllLocations();
+            return Ok(allLocations);
+        }
+
+        [HttpGet("throw")]
+        public IEnumerable<Location> Throw()
+        {
+            throw new InvalidOperationException($"This is a test exception: {DateTime.Now}");
+        }
+
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult GetLocationById(int id)
+        {
+            var location = _unitOfWork.Locations.GetLocationById(id);
+            return Ok(location);
+        }
+
+
+
+        // POST api/values
+        [HttpPost("addLocation")]
+        public IActionResult Post([FromBody] Location data)
+        {
+            _unitOfWork.Locations.AddLocation(data);
+            _unitOfWork.SaveChanges();
+            return Ok(data);
+        }
+
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Location data)
+        {
+            var locationToUpdate = _unitOfWork.Locations.GetLocationById(id);
+
+            if (locationToUpdate != null)
+            {
+                try
+                {
+                    locationToUpdate.Title = data.Title;
+                    locationToUpdate.Address = data.Address;
+                    locationToUpdate.Courts = data.Courts;
+
+                    _unitOfWork.Locations.UpdateLocation(locationToUpdate);
+                    _unitOfWork.SaveChanges();
+                    return Ok(data);
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, $"an error occured: {ex.Message}");
+                }
+            }
+            return NotFound();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var location = _unitOfWork.Locations.Get(id);
+            _unitOfWork.Locations.Remove(location);
+            _unitOfWork.SaveChanges();
+            return NoContent();
+        }
+    }
+}
